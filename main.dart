ed30129bece80b45be57318bca37e2f1bbbc8d5f
Ks3216 <104560029+Ks3216@users.noreply.github.com>
2024-07-07 17:25:49 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,92 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import 'package:loggy/loggy.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:uuid/uuid.dart';
+
+import 'package:taskwarrior/routes/pageroute.dart';
+import 'package:taskwarrior/services/notification_services.dart';
+import 'package:taskwarrior/views/home/home.dart';
+import 'package:taskwarrior/views/profile/profile.dart';
+import 'package:taskwarrior/widgets/pallete.dart';
+import 'package:taskwarrior/widgets/taskdetails/profiles_widget.dart';
+
+// import 'package:taskwarrior/model/task.dart';
+//import 'package:flutter_dotenv/flutter_dotenv.dart'
+
+Future main([List<String> args = const []]) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Permission.notification.isDenied.then((value) {
+    if (value) {
+      Permission.notification.request();
+    }
+  });
+
+  Directory? testingDirectory;
+  if (args.contains('flutter_driver_test')) {
+    testingDirectory = Directory(
+      '${Directory.systemTemp.path}/flutter_driver_test/${const Uuid().v1()}',
+    )..createSync(recursive: true);
+    stdout.writeln(testingDirectory);
+    Directory(
+      '${testingDirectory.path}/profiles/acae0462-6a34-11e4-8001-002590720087',
+    ).createSync(recursive: true);
+  }
+
+  runApp(
+    FutureBuilder<Directory>(
+      future: getApplicationDocumentsDirectory(),
+      builder: (context, snapshot) => (snapshot.hasData)
+          ? ProfilesWidget(
+              baseDirectory: testingDirectory ?? snapshot.data!,
+              child: const MyApp(),
+            )
+          : const Placeholder(),
+    ),
+  );
+}
+
+Future init() async {
+  Loggy.initLoggy(logPrinter: const PrettyPrinter());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyAppState createState() => _MyAppState();
+}
+
+// ignore: use_key_in_widget_constructors
+class _MyAppState extends State<MyApp> {
+  NotificationService notificationService = NotificationService();
+  @override
+  void initState() {
+    super.initState();
+
+    notificationService.initiliazeNotification();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Taskwarrior',
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Palette.kToDark,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: PageRoutes.home,
+      routes: {
+        PageRoutes.home: (context) => HomePage(),
+        PageRoutes.profile: (context) => const ProfilePage(),
+      },
+    );
+  }
+}
---
+++ project_filter.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:taskwarrior/config/app_settings.dart';
+import 'package:taskwarrior/widgets/taskw.dart';
+
+class InheritedProjects extends InheritedWidget {
+  const InheritedProjects({
+    required this.projects,
+    required this.projectFilter,
+    required this.callback,
+    required child,
+    Key? key,
+  }) : super(key: key, child: child);
+
+  final Map<String, ProjectNode> projects;
+  final String projectFilter;
+  final void Function(String) callback;
+
+  static InheritedProjects of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<InheritedProjects>()!;
+  }
+
+  @override
+  bool updateShouldNotify(InheritedProjects oldWidget) =>
+      projectFilter != oldWidget.projectFilter ||
+      projects != oldWidget.projects ||
+      callback != oldWidget.callback;
+}
+
+class ProjectsColumn extends StatelessWidget {
+  const ProjectsColumn(this.projects, this.projectFilter, this.callback,
+      {Key? key})
+      : super(key: key);
+
+  final Map<String, ProjectNode> projects;
+  final String projectFilter;
+  final void Function(String) callback;
+
+  @override
+  Widget build(BuildContext context) {
+    return InheritedProjects(
+      projectFilter: projectFilter,
+      callback: callback,
+      projects: projects,
+      child: ExpansionTile(
+        key: const PageStorageKey('project-filter'),
+        title: Text(
+          'project:$projectFilter',
+          style: GoogleFonts.firaMono(
+            color: AppSettings.isDarkMode ? Colors.white : Colors.black,
+            fontSize: 18,
+          ),
+        ),
+        backgroundColor: AppSettings.isDarkMode
+            ? const Color.fromARGB(255, 48, 46, 46)
+            : const Color.fromARGB(255, 220, 216, 216),
+        iconColor: AppSettings.isDarkMode ? Colors.white : Colors.black,
+        collapsedIconColor:
+            AppSettings.isDarkMode ? Colors.white : Colors.black,
+        children: (Map.of(projects)
+              ..removeWhere((_, nodeData) => nodeData.parent != null))
+            .keys
+            .map(ProjectTile.new)
+            .toList(),
+      ),
+    );
+  }
+}
+
+class ProjectTile extends StatelessWidget {
+  const ProjectTile(this.project, {Key? key}) : super(key: key);
+
+  final String project;
+
+  @override
+  Widget build(BuildContext context) {
+    var inheritedProjects = InheritedProjects.of(context);
+
+    var node = inheritedProjects.projects[project]!;
+    var projectFilter = inheritedProjects.projectFilter;
+    var callback = inheritedProjects.callback;
+
+    var title = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Flexible(
+            child: Text(project,
+                style: GoogleFonts.firaMono(
+                    color:
+                        AppSettings.isDarkMode ? Colors.white : Colors.black))),
+        Text(
+          (node.children.isEmpty)
+              ? '${node.subtasks}'
+              : '(${node.tasks}) ${node.subtasks}',
+          style: GoogleFonts.firaMono(
+              color: AppSettings.isDarkMode ? Colors.white : Colors.black),
+        )
+      ],
+    );
+
+    var radio = Radio(
+      activeColor: AppSettings.isDarkMode
+          ? Colors.white
+          : const Color.fromARGB(255, 48, 46, 46),
+      focusColor: AppSettings.isDarkMode
+          ? Colors.white
+          : const Color.fromARGB(255, 48, 46, 46),
+      toggleable: true,
+      value: project,
+      groupValue: projectFilter,
+      onChanged: (_) => callback(project),
+    );
+
+    return (node.children.isEmpty)
+        ? ListTile(
+            leading: radio,
+            title: title,
+            onTap: () => callback(project),
+            tileColor: AppSettings.isDarkMode
+                ? const Color.fromARGB(255, 48, 46, 46)
+                : const Color.fromARGB(255, 220, 216, 216),
+            textColor: AppSettings.isDarkMode
+                ? Colors.white
+                : const Color.fromARGB(255, 48, 46, 46),
+          )
+        : ExpansionTile(
+            // textColor: Theme.of(context).textTheme.subtitle1!.color,
+            controlAffinity: ListTileControlAffinity.leading,
+            leading: radio,
+            title: title,
+            backgroundColor: AppSettings.isDarkMode
+                ? const Color.fromARGB(255, 48, 46, 46)
+                : const Color.fromARGB(255, 220, 216, 216),
+            textColor: AppSettings.isDarkMode
+                ? Colors.white
+                : const Color.fromARGB(255, 48, 46, 46),
+            children: node.children.map(ProjectTile.new).toList(),
+          );
+  }
+}
